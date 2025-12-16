a61bce8f7469ed2af33b262e6c35cfa2483c8f2b
Doni Setiawan <d.setiawan2255@gmail.com>
2025-12-16 12:05:59 +0700

    Delete UIN-AKSI directory

--- UIN-AKSI/src/types/term.ts
+++
@@ -1,9 +0,0 @@
-export interface Term {
-    id: string;
-    title: string;
-    description: string;
-    category: string;
-    image_url: string;
-    user_id: string;
-    created_at: string;
-}
